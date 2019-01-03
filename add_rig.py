741c121aae7838830d6ee3a2f8c4831eda94c3b8
danielenger <35110574+danielenger@users.noreply.github.com>
2019-01-03 19:32:27 +0100

    Add files via upload

--- add_rig.py
+++ add_rig.py
@@ -1,7 +1,64 @@
 import bpy
 from mathutils import Vector
+import math
 
-# from . rigifymetarigformblab_functions import *
+metarig_bone_names_legacy_mode = { # metarig_bone : mblab_bone
+    "hips":"pelvis",
+    "spine":"spine01",
+    # "chest":"", from spine01 tail to neck head
+    "neck":"neck",
+    "head":"head",
+
+    # "breast.L":"breast_L",
+    "foot.L":"foot_L",
+    "toe.L":"toes_L",
+    "shoulder.L":"clavicle_L",
+    "hand.L":"hand_L",
+    "thumb.01.L":"thumb01_L",
+    "thumb.02.L":"thumb02_L",
+    "thumb.03.L":"thumb03_L",
+    "palm.01.L":"index00_L",
+    "f_index.01.L":"index01_L",
+    "f_index.02.L":"index02_L",
+    "f_index.03.L":"index03_L",
+    "palm.02.L":"middle00_L",
+    "f_middle.01.L":"middle01_L",
+    "f_middle.02.L":"middle02_L",
+    "f_middle.03.L":"middle03_L",
+    "palm.03.L":"ring00_L",
+    "f_ring.01.L":"ring01_L",
+    "f_ring.02.L":"ring02_L",
+    "f_ring.03.L":"ring03_L",
+    "palm.04.L":"pinky00_L",
+    "f_pinky.01.L":"pinky01_L",
+    "f_pinky.02.L":"pinky02_L",
+    "f_pinky.03.L":"pinky03_L",
+
+    # "breast.R":"breast_R",
+    "foot.R":"foot_R",
+    "toe.R":"toes_R",
+    "shoulder.R":"clavicle_R",
+    "hand.R":"hand_R",
+    "thumb.01.R":"thumb01_R",
+    "thumb.02.R":"thumb02_R",
+    "thumb.03.R":"thumb03_R",
+    "palm.01.R":"index00_R",
+    "f_index.01.R":"index01_R",
+    "f_index.02.R":"index02_R",
+    "f_index.03.R":"index03_R",
+    "palm.02.R":"middle00_R",
+    "f_middle.01.R":"middle01_R",
+    "f_middle.02.R":"middle02_R",
+    "f_middle.03.R":"middle03_R",
+    "palm.03.R":"ring00_R",
+    "f_ring.01.R":"ring01_R",
+    "f_ring.02.R":"ring02_R",
+    "f_ring.03.R":"ring03_R",
+    "palm.04.R":"pinky00_R",
+    "f_pinky.01.R":"pinky01_R",
+    "f_pinky.02.R":"pinky02_R",
+    "f_pinky.03.R":"pinky03_R",
+}
 
 metarig_bone_names = { # metarig_bone : mblab_bone
     "spine":"pelvis",
@@ -62,6 +119,7 @@ metarig_bone_names = { # metarig_bone : mblab_bone
     "f_pinky.02.R":"pinky02_R",
     "f_pinky.03.R":"pinky03_R",
 }
+
 metarig_bone_arm_names = { # metarig_bone : mblab_bone
     "upper_arm.L":"upperarm_L",
     "forearm.L":"lowerarm_L",
@@ -95,11 +153,12 @@ class RigifyMetaRigForMBLab_OT_add_rig(bpy.types.Operator):
 
 
     def execute(self, context):
-        # self.settings = context.scene.rigifymetarigformblab_settings
         
         mblab_rig = context.active_object
 
         meta_rig_bone_data = {} # store vectors for head, tail and roll for every meta bone -> "name of meta rig bone" : (head, tail, roll)
+
+        legacy_mode = True if bpy.context.preferences.addons['rigify'].preferences['legacy_mode'] == 1 else False
 
         # error handling: a rig needs to be active
         if not mblab_rig.type == 'ARMATURE':
@@ -111,9 +170,15 @@ class RigifyMetaRigForMBLab_OT_add_rig(bpy.types.Operator):
         ########
         bpy.ops.object.mode_set(mode='EDIT')
         # get bone data from mblab rig and store vectors (head, tail, roll) in meta_rig_bone_data dictionary
-        for metarig_bone, mblab_bone in metarig_bone_names.items():
-            b = mblab_rig.data.edit_bones[mblab_bone]
-            meta_rig_bone_data[metarig_bone] = (b.head.copy(), b.tail.copy(), b.roll)
+        if legacy_mode:
+            for metarig_bone, mblab_bone in metarig_bone_names_legacy_mode.items():
+                b = mblab_rig.data.edit_bones[mblab_bone]
+                meta_rig_bone_data[metarig_bone] = (b.head.copy(), b.tail.copy(), b.roll)
+        else:
+            for metarig_bone, mblab_bone in metarig_bone_names.items():
+                b = mblab_rig.data.edit_bones[mblab_bone]
+                meta_rig_bone_data[metarig_bone] = (b.head.copy(), b.tail.copy(), b.roll)
+
         # repeat for arm bones
         for metarig_bone, mblab_bone in metarig_bone_arm_names.items():
             b = mblab_rig.data.edit_bones[mblab_bone]
@@ -130,8 +195,6 @@ class RigifyMetaRigForMBLab_OT_add_rig(bpy.types.Operator):
                 # new knee position for straight legs                    
                 tmp_thigh_head = mblab_rig.data.edit_bones[mblab_thigh_name].head.copy()
                 tmp_thigh_tail = mblab_rig.data.edit_bones[mblab_shin_name].tail.copy()
-                tmp_thigh_roll = mblab_rig.data.edit_bones[mblab_thigh_name].roll
-                tmp_shin_roll = mblab_rig.data.edit_bones[mblab_shin_name].roll
                 new_length = mblab_rig.data.edit_bones[mblab_thigh_name].length 
                 new_straight_legs_knee_pos = new_length * ( (tmp_thigh_tail - tmp_thigh_head).normalized() ) + tmp_thigh_head
                 # y from mblab knee
@@ -139,38 +202,63 @@ class RigifyMetaRigForMBLab_OT_add_rig(bpy.types.Operator):
                 new_knee_pos = new_straight_legs_knee_pos
                 new_knee_pos.y = new_straight_legs_knee_pos.y + self.knee_offset_y
 
+                tmp_thigh_roll = mblab_rig.data.edit_bones[mblab_thigh_name].roll
+                tmp_shin_roll = mblab_rig.data.edit_bones[mblab_shin_name].roll
+
+                # legacy_mode legs roll
+                if legacy_mode:
+                    tmp_thigh_roll = tmp_thigh_roll + math.pi
+                    tmp_shin_roll = tmp_shin_roll + math.pi
+
+                    print("tmp_thigh_roll = ", tmp_thigh_roll)
+                    print("tmp_shin_roll = ", tmp_shin_roll)
+                    print(math.pi)
+                    
                 meta_rig_bone_data[thigh_name] = (tmp_thigh_head, new_knee_pos, tmp_thigh_roll)
                 meta_rig_bone_data[shin_name] = (new_knee_pos, tmp_thigh_tail, tmp_shin_roll)
         else:
             for metarig_bone, mblab_bone in metarig_bone_leg_names.items():
                 b = mblab_rig.data.edit_bones[mblab_bone]
                 meta_rig_bone_data[metarig_bone] = (b.head.copy(), b.tail.copy(), b.roll)
-
-
-        # x location for metarig heels from mblab "foot_L/R" head
-        foot_l_x = mblab_rig.data.edit_bones["foot_L"].head.x
-        foot_r_x = mblab_rig.data.edit_bones["foot_R"].head.x
-
+        
 
         # location for metarig "spine.004" head and "spine.005" head
-        tmp_neck_bone = mblab_rig.data.edit_bones["neck"]
+        if not legacy_mode:
+            tmp_neck_bone = mblab_rig.data.edit_bones["neck"]
 
-        spine005_head = tmp_neck_bone.length/2 * ( (tmp_neck_bone.tail - tmp_neck_bone.head).normalized() ) + tmp_neck_bone.head
-        spine005_tail = tmp_neck_bone.tail.copy()
-        spine005_roll = tmp_neck_bone.roll
+            spine005_head = tmp_neck_bone.length/2 * ( (tmp_neck_bone.tail - tmp_neck_bone.head).normalized() ) + tmp_neck_bone.head
+            spine005_tail = tmp_neck_bone.tail.copy()
+            spine005_roll = tmp_neck_bone.roll
 
-        spine004_head = tmp_neck_bone.head.copy()
-        spine004_tail = spine005_head
-        spine004_roll = tmp_neck_bone.roll
+            spine004_head = tmp_neck_bone.head.copy()
+            spine004_tail = spine005_head
+            spine004_roll = tmp_neck_bone.roll
 
-        meta_rig_bone_data["spine.005"] = (spine005_head, spine005_tail, spine005_roll)
-        meta_rig_bone_data["spine.004"] = (spine004_head, spine004_tail, spine004_roll)
+            meta_rig_bone_data["spine.005"] = (spine005_head, spine005_tail, spine005_roll)
+            meta_rig_bone_data["spine.004"] = (spine004_head, spine004_tail, spine004_roll)
 
-        
+        # legacy_mode chest
+        if legacy_mode:
+            # chest_tail = neck_head
+            chest_head = mblab_rig.data.edit_bones["spine02"].head.copy()
+            chest_tail = mblab_rig.data.edit_bones["neck"].head.copy()
+            chest_roll = mblab_rig.data.edit_bones["spine02"].roll
+            meta_rig_bone_data["chest"] = (chest_head, chest_tail, chest_roll)
+
+        # legacy_mode breast bones data
+        if legacy_mode:
+            for ext in ["L","R"]:
+                bone_name = "breast_" + ext
+                breast_head = mblab_rig.data.edit_bones[bone_name].head.copy()
+                breast_tail = mblab_rig.data.edit_bones[bone_name].tail.copy()
+                breast_roll = mblab_rig.data.edit_bones[bone_name].roll
+                meta_rig_bone_data["breast." + ext] = (breast_head, breast_tail, breast_roll)
+
+
+        bpy.ops.object.mode_set(mode='OBJECT')
         ########
         # END edit mode for mblab armature
         ########
-        bpy.ops.object.mode_set(mode='OBJECT')
         
 
         # create meta rig
@@ -178,61 +266,70 @@ class RigifyMetaRigForMBLab_OT_add_rig(bpy.types.Operator):
         bpy.ops.object.armature_human_metarig_add()
         meta_rig = context.active_object
         meta_rig.location = mblab_rig.location
-        
+
 
         ########
         # START edit mode for meta-rig
         ########
         bpy.ops.object.mode_set(mode='EDIT')
-        
-        # metarig heels            
-        foot_l_x = meta_rig_bone_data["foot.L"][0][0] # mblab_rig.data.edit_bones["foot_L"].head.x
-        foot_r_x = meta_rig_bone_data["foot.R"][0][0] # mblab_rig.data.edit_bones["foot_R"].head.x
+
+        # legacy_mode add breast bones
+        if legacy_mode:
+            for ext in ["L","R"]:
+                bone_name = "breast." + ext
+                bpy.context.active_object.data.edit_bones.new(bone_name)
+                bpy.context.active_object.data.edit_bones[bone_name].parent = bpy.context.active_object.data.edit_bones["chest"]
+                
+
         # y and z from mblab object origin is the same for left and right
-        heel_y = mblab_rig.location.y
-        heel_z = mblab_rig.location.z            
-        bone_name = "heel.02.L"
-        bone_heel_l = meta_rig.data.edit_bones[bone_name]
-        # metarig heel x location relative in middle to mblab foot head
-        heel_l_head_x = (bone_heel_l.head.x - bone_heel_l.tail.x)/2 + foot_l_x
-        heel_l_tail_x = (bone_heel_l.tail.x - bone_heel_l.head.x)/2 + foot_l_x
-        # create new vectors from collected data
-        bone_heel_l_head = Vector( (heel_l_head_x, heel_y, heel_z) )
-        bone_heel_l_tail = Vector( (heel_l_tail_x, heel_y, heel_z) )
-        bone_heel_l_roll = meta_rig.data.edit_bones[bone_name].roll
-        # store data
-        meta_rig_bone_data[bone_name] = (bone_heel_l_head, bone_heel_l_tail, bone_heel_l_roll)
-        
-        bone_name = "heel.02.R"
-        bone_heel_r = meta_rig.data.edit_bones[bone_name]
-        # metarig heel x location relative in middle to mblab foot head
-        heel_r_head_x = (bone_heel_r.head.x - bone_heel_r.tail.x)/2 + foot_r_x
-        heel_r_tail_x = (bone_heel_r.tail.x - bone_heel_r.head.x)/2 + foot_r_x
-        # create new vectors from collected data
-        bone_heel_r_head = Vector( (heel_r_head_x, heel_y, heel_z) )
-        bone_heel_r_tail = Vector( (heel_r_tail_x, heel_y, heel_z) )
-        bone_heel_r_roll = meta_rig.data.edit_bones[bone_name].roll
-        # store data
-        meta_rig_bone_data[bone_name] = (bone_heel_r_head, bone_heel_r_tail, bone_heel_r_roll)
+        heel_02_y = mblab_rig.location.y
+        heel_02_z = mblab_rig.location.z
+
+        # "heel.02.L", "heel.02.R"
+        for ext in [".L",".R"]:
+            bone_name = "heel.02" + ext
+            bone_heel_02 = meta_rig.data.edit_bones[bone_name]
+            # metarig heel x location relative in middle to mblab foot head
+            heel_02_head_x = (bone_heel_02.head.x - bone_heel_02.tail.x)/2 + meta_rig_bone_data["foot" + ext][0][0] # foot_l/r_x
+            heel_02_tail_x = (bone_heel_02.tail.x - bone_heel_02.head.x)/2 + meta_rig_bone_data["foot" + ext][0][0] # foot_l/r_x
+            # create new vectors from collected data
+            bone_heel_02_head = Vector( (heel_02_head_x, heel_02_y, heel_02_z) )
+            bone_heel_02_tail = Vector( (heel_02_tail_x, heel_02_y, heel_02_z) )
+            bone_heel_02_roll = meta_rig.data.edit_bones[bone_name].roll
+            # store data
+            meta_rig_bone_data[bone_name] = (bone_heel_02_head, bone_heel_02_tail, bone_heel_02_roll)
+
+            # legacy_mode heels:
+            if legacy_mode:
+                # bone_name = "foot" + ext
+                heel_head = meta_rig_bone_data["foot" + ext][0] #meta_rig.data.edit_bones["foot" + ext].head.copy()
+                # bone_name = "heel" + ext
+                heel_tail = meta_rig.data.edit_bones["heel" + ext].tail.copy()
+                heel_tail[0] = heel_head[0] # heel.tail.x = heel.head.x
+                heel_tail[1] = mblab_rig.location.y # heel.tail.y = 0
+                meta_rig_bone_data["heel" + ext] = (heel_head, heel_tail, 0)
 
         # meta rig pelvis
-        new_pelvis_head = meta_rig_bone_data["spine"][0] # mblab_rig.data.edit_bones["pelvis"].head.copy()
-        tmp_bone_list = ("pelvis.L", "pelvis.R")
-        for bone_name in tmp_bone_list:
-            pelvis_head = meta_rig.data.edit_bones[bone_name].head
-            pelvis_tail = meta_rig.data.edit_bones[bone_name].tail
-            new_pelvis_tail = (pelvis_tail - pelvis_head) + new_pelvis_head
-            new_pelvis_roll = meta_rig.data.edit_bones[bone_name].roll            
-            meta_rig_bone_data[bone_name] = (new_pelvis_head, new_pelvis_tail, new_pelvis_roll)
+        if not legacy_mode:
+            new_pelvis_head = meta_rig_bone_data["spine"][0] # mblab_rig.data.edit_bones["pelvis"].head.copy()
+            tmp_bone_list = ("pelvis.L", "pelvis.R")
+            for bone_name in tmp_bone_list:
+                pelvis_head = meta_rig.data.edit_bones[bone_name].head
+                pelvis_tail = meta_rig.data.edit_bones[bone_name].tail
+                new_pelvis_tail = (pelvis_tail - pelvis_head) + new_pelvis_head
+                new_pelvis_roll = meta_rig.data.edit_bones[bone_name].roll            
+                meta_rig_bone_data[bone_name] = (new_pelvis_head, new_pelvis_tail, new_pelvis_roll)
         
         # meta rig face rig
-        relative_offset = meta_rig.data.edit_bones["face"].head.copy() - meta_rig_bone_data["spine.006"][0]
-        for bone in meta_rig.data.edit_bones["face"].parent.children_recursive:                
-            bone_name = bone.name
-            new_h = bone.head.copy() - relative_offset
-            new_t = bone.tail.copy() - relative_offset
-            new_r = meta_rig.data.edit_bones[bone_name].roll
-            meta_rig_bone_data[bone_name] = (new_h, new_t, new_r)
+        if not legacy_mode:
+            relative_offset = meta_rig.data.edit_bones["face"].head.copy() - meta_rig_bone_data["spine.006"][0]
+            for bone in meta_rig.data.edit_bones["face"].parent.children_recursive:                
+                bone_name = bone.name
+                new_h = bone.head.copy() - relative_offset
+                new_t = bone.tail.copy() - relative_offset
+                new_r = meta_rig.data.edit_bones[bone_name].roll
+                meta_rig_bone_data[bone_name] = (new_h, new_t, new_r)
+
             
         # go through all bones in meta rig, pass the ones not needed
         for b in meta_rig.data.edit_bones:
@@ -244,10 +341,25 @@ class RigifyMetaRigForMBLab_OT_add_rig(bpy.types.Operator):
             except:
                 pass
         
+
+        # legacy_mode finger roll
+        for ext in [".L",".R"]:
+            for bone in meta_rig.data.edit_bones["hand" + ext].children_recursive:
+                bone.roll = bone.roll + math.pi
+
+
+        bpy.ops.object.mode_set(mode='OBJECT')
         ########
         # END edit mode for meta-rig
         ########
-        bpy.ops.object.mode_set(mode='OBJECT')
-        
+
+
+        # legacy_mode breast bones - set bone types and layers
+        if legacy_mode:
+            for ext in ["L","R"]:
+                bone_name = "breast." + ext
+                bpy.context.active_object.pose.bones[bone_name].rigify_type = "basic.copy"
+                bpy.context.active_object.data.bones[bone_name].layers = bpy.context.active_object.data.bones["chest"].layers
+
 
         return {'FINISHED'}
--- rename_vertex_groups.py
+++ rename_vertex_groups.py
@@ -1,5 +1,74 @@
 import bpy
 
+metarig_bone_names_legacy_mode = { # metarig_bone : mblab_bone
+    "hips": "pelvis",
+    "spine":"spine01",
+    "chest":"spine02",
+    "neck":"neck",
+    "head":"head",
+
+    "breast.L":"breast_L",
+    "foot.L":"foot_L",
+    "toe.L":"toes_L",
+    "shoulder.L":"clavicle_L",
+
+    "hand.L":"hand_L",
+    "thumb.01.L.02":"thumb01_L",
+    "thumb.02.L":"thumb02_L",
+    "thumb.03.L":"thumb03_L",
+
+    "palm.01.L":"index00_L",
+    "f_index.01.L.02":"index01_L",
+    "f_index.02.L":"index02_L",
+    "f_index.03.L":"index03_L",
+
+    "palm.02.L":"middle00_L",
+    "f_middle.01.L.02":"middle01_L",
+    "f_middle.02.L":"middle02_L",
+    "f_middle.03.L":"middle03_L",
+
+    "palm.03.L":"ring00_L",
+    "f_ring.01.L.02":"ring01_L",
+    "f_ring.02.L":"ring02_L",
+    "f_ring.03.L":"ring03_L",
+
+    "palm.04.L":"pinky00_L",
+    "f_pinky.01.L.02":"pinky01_L",
+    "f_pinky.02.L":"pinky02_L",
+    "f_pinky.03.L":"pinky03_L",
+
+    "breast.R":"breast_R",
+    "foot.R":"foot_R",
+    "toe.R":"toes_R",
+    "shoulder.R":"clavicle_R",
+
+    "hand.R":"hand_R",
+    "thumb.01.R.02":"thumb01_R",
+    "thumb.02.R":"thumb02_R",
+    "thumb.03.R":"thumb03_R",
+
+    "palm.01.R":"index00_R",
+    "f_index.01.R.02":"index01_R",
+    "f_index.02.R":"index02_R",
+    "f_index.03.R":"index03_R",
+
+    "palm.02.R":"middle00_R",
+    "f_middle.01.R.02":"middle01_R",
+    "f_middle.02.R":"middle02_R",
+    "f_middle.03.R":"middle03_R",
+
+    "palm.03.R":"ring00_R",
+    "f_ring.01.R.02":"ring01_R",
+    "f_ring.02.R":"ring02_R",
+    "f_ring.03.R":"ring03_R",
+
+    "palm.04.R":"pinky00_R",
+    "f_pinky.01.R.02":"pinky01_R",
+    "f_pinky.02.R":"pinky02_R",
+    "f_pinky.03.R":"pinky03_R",
+
+}
+
 metarig_bone_names = { # metarig_bone : mblab_bone
     "spine":"pelvis",
     "spine.001":"spine01",
@@ -61,28 +130,37 @@ metarig_bone_names = { # metarig_bone : mblab_bone
 }
 
 mblab_bone_names = { # mblab_bone : DEF-metarig_bone - for rename only
-    "upperarm_twist_L":"DEF-upper_arm.L",
-    "upperarm_L":"DEF-upper_arm.L.001",
-    "lowerarm_twist_L":"DEF-forearm.L.001",
-    "lowerarm_L":"DEF-forearm.L",
-    "thigh_twist_L":"DEF-thigh.L",
-    "thigh_L":"DEF-thigh.L.001",
-    "calf_twist_L":"DEF-shin.L.001",
-    "calf_L":"DEF-shin.L",
-    "upperarm_twist_R":"DEF-upper_arm.R",
-    "upperarm_R":"DEF-upper_arm.R.001",
-    "lowerarm_twist_R":"DEF-forearm.R.001",
-    "lowerarm_R":"DEF-forearm.R",
-    "thigh_twist_R":"DEF-thigh.R",
-    "thigh_R":"DEF-thigh.R.001",
-    "calf_twist_R":"DEF-shin.R.001",
-    "calf_R":"DEF-shin.R",
+    "upperarm_twist_L":"DEF-upper_arm.01.L",
+    "upperarm_L":"DEF-upper_arm.02.L",
+    "lowerarm_twist_L":"DEF-forearm.01.L",
+    "lowerarm_L":"DEF-forearm.02.L",
+    "thigh_twist_L":"DEF-thigh.01.L",
+    "thigh_L":"DEF-thigh.02.L",
+    "calf_twist_L":"DEF-shin.01.L",
+    "calf_L":"DEF-shin.02.L",
+
+    "upperarm_twist_R":"DEF-upper_arm.01.R",
+    "upperarm_R":"DEF-upper_arm.02.R",
+    "lowerarm_twist_R":"DEF-forearm.01.R",
+    "lowerarm_R":"DEF-forearm.02.R",
+    "thigh_twist_R":"DEF-thigh.01.R",
+    "thigh_R":"DEF-thigh.02.R",
+    "calf_twist_R":"DEF-shin.01.R",
+    "calf_R":"DEF-shin.02.R",
 }
 
 # add bone names from metarig_bone_names dict and add renamed to mblab_bone_names dict
 def update_mblab_bone_names():
-    for metarig_bone, mblab_bone in metarig_bone_names.items():
-        mblab_bone_names[mblab_bone] = "DEF-" + metarig_bone
+
+    legacy_mode = True if bpy.context.preferences.addons['rigify'].preferences['legacy_mode'] == 1 else False
+
+    if legacy_mode:
+        for metarig_bone, mblab_bone in metarig_bone_names_legacy_mode.items():
+            mblab_bone_names[mblab_bone] = "DEF-" + metarig_bone
+    else:
+        for metarig_bone, mblab_bone in metarig_bone_names.items():
+            mblab_bone_names[mblab_bone] = "DEF-" + metarig_bone
+
 
 class RigifyMetaRigForMBLab_OT_rename_mblab_to_rigify(bpy.types.Operator):
     """Rename Vertex Groups to match Rigify Rig
@@ -90,19 +168,22 @@ class RigifyMetaRigForMBLab_OT_rename_mblab_to_rigify(bpy.types.Operator):
     bl_idname = "object.rigify_meta_rig_for_mblab_rename_mblab_to_rigify"
     bl_label = "MB-Lab to Rigify"
     bl_options = {'REGISTER', 'UNDO'}
-    
+
     def execute(self, context):
-        mblab_mesh = context.active_object
-        if mblab_mesh.type == 'MESH':
-            update_mblab_bone_names()
-            # go through mblab_bone_names dictinary and try rename vertex groups
-            for mblab_bone, metarig_bone in mblab_bone_names.items():
-                try:
-                    mblab_mesh.vertex_groups[mblab_bone].name = metarig_bone
-                except:
-                    print("Error: '%s' could not be found in vertex groups" % mblab_bone)
-        else:
-            print("Error: '%s' is not a mesh object" % mblab_mesh.name)
+
+        # error handling
+        if not context.active_object.type == 'MESH':
+            self.report({'ERROR'}, "Error: '{0}' is not a mesh object".format(context.active_object.name))
+            return {'CANCELLED'}
+            
+        update_mblab_bone_names()
+        # go through mblab_bone_names dictinary and try rename vertex groups
+        for mblab_bone, metarig_bone in mblab_bone_names.items():
+            try:
+                context.active_object.vertex_groups[mblab_bone].name = metarig_bone
+            except:
+                print("Error: '%s' could not be found in vertex groups" % mblab_bone)
+    
         return {'FINISHED'}
 
 class RigifyMetaRigForMBLab_OT_rename_rigify_to_mblab(bpy.types.Operator):
@@ -113,15 +194,18 @@ class RigifyMetaRigForMBLab_OT_rename_rigify_to_mblab(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
     
     def execute(self, context):
-        mblab_mesh = context.active_object
-        if mblab_mesh.type == 'MESH':
-            update_mblab_bone_names()
-            # go through mblab_bone_names dictinary and try rename vertex groups
-            for mblab_bone, metarig_bone in mblab_bone_names.items():
-                try:
-                    mblab_mesh.vertex_groups[metarig_bone].name = mblab_bone
-                except:
-                    print("Error: '%s' could not be found in vertex groups" % metarig_bone)
-        else:
-            print("Error: '%s' is not a mesh object" % mblab_mesh.name)
+
+        # error handling
+        if not context.active_object.type == 'MESH':
+            self.report({'ERROR'}, "Error: '{0}' is not a mesh object".format(context.active_object.name))
+            return {'CANCELLED'}
+
+        update_mblab_bone_names()
+        # go through mblab_bone_names dictinary and try rename vertex groups
+        for mblab_bone, metarig_bone in mblab_bone_names.items():
+            try:
+                context.active_object.vertex_groups[metarig_bone].name = mblab_bone
+            except:
+                print("Error: '%s' could not be found in vertex groups" % metarig_bone)
+        
         return {'FINISHED'}
